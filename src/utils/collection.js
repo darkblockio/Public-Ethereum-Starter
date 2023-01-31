a10817f0d8a8672c52ed10dd410aabe0ec8138b2
gstrader28 <gstrader28@yahoo.com>
2023-01-31 13:07:57 -0600

    Initial commit

---
+++ src/utils/collection.js
@@ -0,0 +1,13 @@
+
+// Put a nft collection into the array to show in the gallery page.
+export const collection = [
+  {
+    name: 'Tomodachi Key',
+    contract: '0x8088f4612eadb9d60d5c8abf4a9d0fdfc3df2f1e',
+    creator_address: '0x438cba7e454b59a9f897d4731fd3eaef37160c0a',
+    token: '352',
+    creator_name: null,
+    image: 'https://ipfs.io/ipfs/QmZ9VchCqz4syDHtmySP6GbJJpprqKjFSHAqhLyiUcQKwY',
+    is_darkblocked: true,
+  },
+]
